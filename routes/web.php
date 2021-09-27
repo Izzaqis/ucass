3f324cd29fa2c678f279b5e28025d888285493e2
Izzaqis <nizzahqisti@gmail.com>
2021-09-27 20:53:57 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-//use App\Http\Controllers\AdminController;
-//use App\Http\Controllers\UserAllController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +23,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('adminweb.adminHome')->middleware('is_admin');
+Route::resource('/admin/users', UserAllController::class)->middleware('is_admin');
+
 Route::get('/committee/home', [HomeController::class, 'committeeHome'])->name('committeeweb.committeeHome')->middleware('is_admin');
+
 Route::get('/student/home', [HomeController::class, 'studentHome'])->name('studentweb.studentHome')->middleware('is_admin');
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
